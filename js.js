8483e350cce16dc30ae04314093339448ce64274
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-07-24 22:48:03 -0700

    Add JS
    Created object constructor and function that appends new book to array

--- js.js
+++ js.js
@@ -12,3 +12,17 @@ const myLibrary = [
         read: true
     }
 ];
+
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title,author,pages,read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
+
