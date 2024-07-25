e4c698c3798acb45a73088e0d54351279ac1f636
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-07-24 23:16:15 -0700

    Add JS function
    Added display book function

--- js.js
+++ js.js
@@ -17,12 +17,18 @@ function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-}
+    this.read = read
+};
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook);
     console.log(myLibrary);
+}
+
+function displayBook() {
+    myLibrary.forEach((book) => function() {
+        createCard(book.title, book.author, book.pages, book.read);
+    });
 }
 
