6b38629c8e4d2de5f5b8e9e39788a61a8418ce50
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-07-24 17:06:17 -0700

    Edit js
    create book object constructor and function that adds book to array

---
+++ js.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+    };
+}
+
+function addBookToLibrary(title,author,pages, read) {
+    const newBook = new Book(title,author,pages,read);
+    myLibrary.push(newBook)
+}
