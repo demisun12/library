7b572708fc5ec155411c74d566cf077d392e455d
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-07-24 22:34:19 -0700

    Edit HTML and CSS
    Add HTML content and css styling for content and future content

--- js.js
+++ js.js
@@ -1,16 +1,14 @@
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
-    };
-}
-
-function addBookToLibrary(title,author,pages, read) {
-    const newBook = new Book(title,author,pages,read);
-    myLibrary.push(newBook)
-}
+const myLibrary = [
+    {
+        title: "The Silent Patient",
+        author: "Alex Michalides",
+        pages: 323,
+        read: false
+    },
+    {
+        title: "Love Redesigned",
+        author: "Lauren Asher",
+        pages: 514,
+        read: true
+    }
+];
